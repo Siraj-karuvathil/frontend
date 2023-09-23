35192b7ba8ddc29f79be09aa3c974330b341219f
sirajsk <sirajkpd12@gmail.com>
2023-09-23 15:15:21 +0530

    some changes added

--- src/Constants/index.ts
+++ src/Constants/index.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://cooking-academy-backend.onrender.com/api/1.0';
-export const BASE_URL = 'http://localhost:3001/api/1.0';
+export const BASE_URL = 'http://api.cookingacademy.qa/api/1.0';
+// export const BASE_URL = 'http://localhost:3001/api/1.0';
